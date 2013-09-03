e8ba9d4b01f32ed80dee4e18c1dcf636f9315813
Igor Elkin <igvel@mail.ru>
2013-09-03 11:52:41 +0400

    Percolation task completed

--- Algorithms/algs4/algs4-study/Percolation.java
+++ Algorithms/algs4/algs4-study/Percolation.java
@@ -12,10 +12,14 @@ public class Percolation {
     private int N;
 
     /**
-     * Union-find
+     * Union-find for percolation
      */
-    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF percUf;
 
+    /**
+     * Separate Union-find for checks if site is full (to avoid backwash).
+     */
+    private WeightedQuickUnionUF fullUf;
     /**
      * Percolation grid (false - empty, true - full)
      */
@@ -27,7 +31,9 @@ public class Percolation {
     public Percolation(int N) {
         this.N = N;
         // 2 extra slots are used for virtual top (0) and bottom (N + 1)
-        this.uf = new WeightedQuickUnionUF(N * N + 2);
+        this.percUf = new WeightedQuickUnionUF(N * N + 2);
+        // 1 extra slot is used for virtual top (0)
+        this.fullUf = new WeightedQuickUnionUF(N * N + 1);
 
         // Create percolate grid
         // extra row/column is to allow 1..N indexing.
@@ -35,30 +41,43 @@ public class Percolation {
         for (int i = 0; i < grid.length; i++) {
             grid[i] = new boolean[N + 1];
         }
-
-        for (int i = 1; i <= N; i++) {
-            // connect virtual top to all top sites
-            uf.union(0, xyTo1D(i, 1));
-            // connect virtual bottom to all bottom sites
-            uf.union(N * N + 1, xyTo1D(i, N));
-        }
     }
 
     /**
      * open site (row i, column j) if it is not already
      */
     public void open(int i, int j) {
-        if(isOpen(i, j)) return;
+        if (isOpen(i, j)) return;
         validateIndices(i, j);
 
         // set open
         grid[i][j] = true;
         // connect open site to neighbor open sites if any
         int idx = xyTo1D(i, j);
-        if(i > 1 && isOpen(i - 1, j)) uf.union(idx, xyTo1D(i - 1, j));
-        if(i < N && isOpen(i + 1, j)) uf.union(idx, xyTo1D(i + 1, j));
-        if(j > 1 && isOpen(i, j - 1)) uf.union(idx, xyTo1D(i, j - 1));
-        if(j < N && isOpen(i, j + 1)) uf.union(idx, xyTo1D(i, j + 1));
+        if (i > 1 && isOpen(i - 1, j)) {
+            percUf.union(idx, xyTo1D(i - 1, j));
+            fullUf.union(idx, xyTo1D(i - 1, j));
+        }
+        if (i < N && isOpen(i + 1, j)) {
+            percUf.union(idx, xyTo1D(i + 1, j));
+            fullUf.union(idx, xyTo1D(i + 1, j));
+        }
+        if (j > 1 && isOpen(i, j - 1)) {
+            percUf.union(idx, xyTo1D(i, j - 1));
+            fullUf.union(idx, xyTo1D(i, j - 1));
+        }
+        if (j < N && isOpen(i, j + 1)) {
+            percUf.union(idx, xyTo1D(i, j + 1));
+            fullUf.union(idx, xyTo1D(i, j + 1));
+        }
+        // connect virtual top to top site
+        if (i == 1) {
+            percUf.union(0, xyTo1D(i, j));
+            fullUf.union(0, xyTo1D(i, j));
+        }
+        // connect virtual bottom to bottom site - only for percolation UF
+        if (i == N) percUf.union(N * N + 1, xyTo1D(i, j));
+
     }
 
     /**
@@ -74,8 +93,8 @@ public class Percolation {
      */
     public boolean isFull(int i, int j) {
         validateIndices(i, j);
-        // is virtual top connect to current site
-        return uf.connected(0, xyTo1D(i, j));
+        // is open and virtual top connect to current site
+        return grid[i][j] && fullUf.connected(0, xyTo1D(i, j));
     }
 
     /**
@@ -83,7 +102,7 @@ public class Percolation {
      */
     public boolean percolates() {
         // is virtual top is connected to virtual bottom
-        return uf.connected(0, N * N  + 1);
+        return percUf.connected(0, N * N  + 1);
     }
 
     /**
@@ -98,7 +117,7 @@ public class Percolation {
      * Converts 2D array coordinates in range [1, N] to 1D array index [1, N*N].
      */
     private int xyTo1D(int i, int j) {
-        return (j - 1) * N + i;
+        return (i - 1) * N + j;
     }
 
     public static void main(String[] args) {
@@ -108,7 +127,7 @@ public class Percolation {
         p.open(2, 2);
         p.open(3, 3);
         p.open(4, 4);
-        System.out.println(p.isFull(2, 2));
-        System.out.println(p.isFull(4, 4));
+        StdOut.println(p.isFull(2, 2));
+        StdOut.println(p.isFull(4, 4));
     }
 }
--- Algorithms/algs4/algs4-study/PercolationStats.java
+++ Algorithms/algs4/algs4-study/PercolationStats.java
@@ -6,18 +6,22 @@
  */
 public class PercolationStats {
 
-    private double[] results;
-    private int T;
+    private double mean;
+    private double stddev;
+    private double confidenceLo;
+    private double confidenceHi;
+
     /**
      * perform T independent computational experiments on an N-by-N grid
      */
     public PercolationStats(int N, int T) {
-        if (N <= 0 || T < 0) throw new IndexOutOfBoundsException("Invalid arguments: N = " + N + ", T = " + T);
+        if (N <= 0 || T <= 0) throw
+                new IllegalArgumentException(
+                        "Invalid arguments: N = " + N + ", T = " + T);
 
-        this.T = T;
-        results = new double[T];
+        double[] results = new double[T];
 
-        for(int t  = 0; t < T; t++) {
+        for (int t  = 0; t < T; t++) {
             Percolation p = new Percolation(N);
             int opened = 0;
             do {
@@ -34,43 +38,50 @@ public class PercolationStats {
             double ratio = ((double) opened) / (N * N);
             results[t] = ratio;
         }
+        mean = StdStats.mean(results);
+        stddev = StdStats.stddev(results);
+        confidenceLo = mean() - 1.96 * stddev() / Math.sqrt(T);
+        confidenceHi = mean() + 1.96 * stddev() / Math.sqrt(T);
     }
 
     /**
      * sample mean of percolation threshold
      */
     public double mean() {
-        return StdStats.mean(results);
+        return mean;
     }
 
     /**
      * sample standard deviation of percolation threshold
      */
     public double stddev() {
-        return StdStats.stddev(results);
+        return stddev;
     }
 
     /**
      * returns lower bound of the 95% confidence interval
      */
     public double confidenceLo() {
-        return mean() - 1.96 * stddev() / Math.sqrt(T);
+        return confidenceLo;
     }
 
     /**
      * returns upper bound of the 95% confidence interval
      */
     public double confidenceHi() {
-        return mean() + 1.96 * stddev() / Math.sqrt(T);
+        return confidenceHi;
     }
 
     /**
      * test client, described below
      */
     public static void main(String[] args) {
-        PercolationStats st = new PercolationStats(200, 100);
-        System.out.println("mean                    =" + st.mean());
-        System.out.println("stddev                  =" + st.stddev());
-        System.out.println("95% confidence interval =" + st.confidenceLo() + "," + st.confidenceHi());
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats st = new PercolationStats(N, T);
+        System.out.println("mean                    = " + st.mean());
+        System.out.println("stddev                  = " + st.stddev());
+        System.out.println("95% confidence interval = "
+                            + st.confidenceLo() + "," + st.confidenceHi());
     }
 }
